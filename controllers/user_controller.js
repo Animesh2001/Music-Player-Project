64b26b19ef4e92ea22932524bcfe8b971c3dac42
Animesh Jaiswal <animesh2jaiswal@gmail.com>
2023-11-06 17:20:27 +0530

    Add files via upload

---
+++ controllers/user_controller.js
@@ -0,0 +1,147 @@
+const User = require('../models/user');
+
+//render the sign up page
+// module.exports.signUp=function(req,res){
+//     return res.render('user_sign_up',{
+//         title: "MastMagan | Sign Up"
+//     })
+// }
+
+
+// //render the sign in page
+// module.exports.signIn=function(req,res){
+//     return res.render('user_sign_in',{
+//         title:"MastMagan | Sign In"
+//     })
+// }
+
+module.exports.create=function(req,res){
+    //Todo later
+    //first check if the password and confirm password mathces or not
+    //if not matches then redirect back to same page
+    if(req.body.password!=req.body.confirm_password){
+        req.flash('error','The password does not matched with confirm password');
+        return res.redirect('back');
+    }
+    //check if email is already present
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){ console.log('error in finding user in signing up'); return;}
+            //when user is not found
+            if(!user){
+                User.create(req.body,function(err,user){
+                    if(err){ console.log('error in creating user while signing up'); return;}
+                    return res.redirect('/users/sign-in'); 
+                })
+            }else{
+                return res.redirect('back');
+            }
+    });
+    
+}
+
+module.exports.profile = function(req,res){
+    res.render('index');
+}
+
+// //get the sign in data
+// module.exports.createSession=function(req,res){
+//     //Todo later
+//     //steps to authenticate 
+//     // console.log("Hello");
+
+//     //find the user
+//     User.findOne({email: req.body.email},function(err,user){
+//         if(err){console.log("error in finding user in signing in"); return;}
+
+
+//     //handle user found
+//     if(user){
+//         //handle password which doesn't match
+//         if(user.password!=req.body.password){
+//             return res.redirect('back');
+//         }
+
+//     //handle session creation
+//     res.cookie('user_id',user.id);
+
+//     res.redirect('/users/profile');
+
+//     }else{
+//             //handle user not found
+//             return res.redirect('back');
+
+//     }
+
+//     });
+
+// }
+//render the sign up page
+module.exports.signUp=function(req,res){
+    //we checked if already signed in then don't show again the sign-up.. instead redirect to profile page
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+    return res.render('user_sign_up',{
+        title:"MastMagan | Sign Up"
+    })
+}
+//render the sign in page and create sesion for user
+module.exports.signIn=function(req,res){
+    //we checked if already signed in then don't show again the sign-in.. instead redirect to profile page
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+    return res.render('sign_in');
+        // title:"MastMagan | Sign In"
+}
+    
+
+module.exports.musicpage=function(req,res){
+    return res.render('index');
+}
+//get the sign in 
+module.exports.createSession=function(req,res){
+    req.flash('success','Loggged in Successfully');
+    return res.redirect('/users');
+}
+
+module.exports.destroySession = function(req,res){
+    //inbuilt function provided by passport to req for logout
+    req.logout();
+    req.flash('success','You have logged out.!');
+
+    return res.redirect('/');
+}
+module.exports.KaranAujla = function(req,res){
+    return res.render('karan_aujla');
+}
+module.exports.SachetTandon = function(req,res){
+    return res.render('sachet_tandon');
+}
+module.exports.NehaKakkar = function(req,res){
+    return res.render('neha');
+}
+
+module.exports.JassiGill = function(req,res){
+    return res.render('jassi');
+}
+module.exports.ArijitSingh = function(req,res){
+    return res.render('arijit');
+}
+module.exports.Badshah = function(req,res){
+    return res.render('badshah');
+}
+
+module.exports.party = function(req,res){
+    return res.render('party');
+}
+
+module.exports.roadtrip = function(req,res){
+    return res.render('roadtrip');
+}
+
+module.exports.electronic = function(req,res){
+    return res.render('electronic');
+}
---
+++ routes/user.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+//remember this 
+const router = express.Router();
+const passport=require('passport');
+const usersController = require('../controllers/user_controller');
+// router.get('/sign-up',usersController.signUp);
+
+router.get('/sign-in',usersController.signIn);
+// router.get('/sign-out',usersController.destroySession);
+
+router.post('/create',usersController.create);
+
+router.get('/',usersController.musicpage);
+
+
+//use passport as a middleware to authenticate
+router.post('/create-session',
+passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'},
+),
+usersController.createSession);
+
+
+router.get('/profile',passport.checkAuthentication,usersController.profile);
+
+router.get('/sign-out', function(req, res, next) {
+    req.logout(function(err) { //req.logout is a passport inbuilt function
+      if (err) { return next(err); }
+      res.redirect('/');
+    })
+  });
+  router.get('/karan-aujla',usersController.KaranAujla);
+  router.get('/sachet-tandon',usersController.SachetTandon);
+  router.get('/neha-kakkar',usersController.NehaKakkar);
+  router.get('/jassi-gill',usersController.JassiGill);
+  
+  router.get('/arijit-singh',usersController.ArijitSingh);
+  
+  router.get('/badshah',usersController.Badshah);
+  
+  router.get('/party-songs',usersController.party);
+  
+  router.get('/electronic-songs',usersController.electronic);
+  
+  router.get('/roadtrip-songs',usersController.roadtrip);
+ 
+
+
+
+
+module.exports=router;
